1a6307b4acb6454554cf0b4d55362319f24032b1
p4r4z1te <thorfinnk322@gmail.com>
2025-12-31 10:54:59 -0500

    Final fix: allowed hosts updated

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p(6i2t+bc@1wdk4mn*_x(j#w^+#&)#n@+%dcej700$37(mqlc)'
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bixgram.onrender.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
